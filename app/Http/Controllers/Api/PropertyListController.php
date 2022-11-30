47bd9839ac1057aa01eafef54214da1523436d0f
Israelkwawu <israel.kwawu@amalitech.com>
2022-11-30 15:25:33 +0000

    Added cursor pagination

---
+++ app/Http/Controllers/Api/PropertyListController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Listing;
+use Illuminate\Http\Request;
+
+
+class PropertyListController extends Controller
+{
+    public function index()
+    { 
+        return response()->json(Listing::cursorPaginate(5));
+    }
+}
