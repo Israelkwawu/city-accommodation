2b1be3a036b61a3e834cea9e246389dec650f23d
Israelkwawu <israel.kwawu@amalitech.com>
2022-12-05 18:06:01 +0000

    Added search module

--- resources/js/store/modules/search.js
+++ resources/js/store/modules/search.js
@@ -2,9 +2,55 @@ import Api from "../../services/user-api";
 export default {
     namespaced: true,
     state: {
+        search_results: [],
+        next_page_url: null,
+        next_cursor: null,
+        error: {},
     },
-    getters: {},
+    getters: {
+        getSearchResults(state) {
+            return state.search_results;
+        },
+        getNextPageUrl(state) {
+            return state.next_page_url;
+        },
+        getNextCursor(state) {
+            return state.next_cursor;
+        },
+        getError(state) {
+            return state.error;
+        }
+
+    },
     mutations: {
+        SET_SEARCH_RESULTS(state, data) {
+            state.search_results = [ ...data ];
+        },
+        SET_NEXT_PAGE_URL(state, data) {
+            state.next_page_url = data;
+        },
+        SET_NEXT_CURSOR(state, data) {
+            state.next_cursor = data;
+        },
+        SET_ERROR(state, data) {
+            state.error = { ...data };
+        },
     },
-    actions: {},
+    actions: {
+        async search({ commit }, payload) {
+            try {
+                const data = await Api().get('/search?cursor='+ payload.next_cursor);
+                commit('SET_SEARCH_RESULTS', data.data.data);
+                commit('SET_NEXT_PAGE_URL', data.data.next_page_url);
+                commit('SET_NEXT_CURSOR', data.data.next_cursor);
+            } catch (error) {
+                commit('SET_ERROR',  {
+                    error: error,
+                    errorMessage: "An unexpected error occurred.",
+                })
+            }
+        
+        }
+
+    },
 }
