4cb45c5536e49d9ede0ae1cc5e8643d20d00725e
Israelkwawu <israel.kwawu@amalitech.com>
2022-12-05 18:05:19 +0000

    Increase search results to 51

--- app/Http/Controllers/Api/PropertyListController.php
+++ app/Http/Controllers/Api/PropertyListController.php
@@ -11,6 +11,6 @@ class PropertyListController extends Controller
 {
     public function index()
     { 
-        return response()->json(Listing::cursorPaginate(6));
+        return response()->json(Listing::cursorPaginate(51));
     }
 }
