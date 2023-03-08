a113ff411440f20485b8941a16e771a86cf13664
Israelkwawu <israel.kwawu@amalitech.com>
2023-03-08 12:02:05 +0000

    axios interceptor

--- resources/js/services/user-api.js
+++ resources/js/services/user-api.js
@@ -1,12 +1,21 @@
 import axios from "axios";
 
+const customAxios = axios.create({
+  baseURL: process.env.MIX_VUE_APP_URL + "/api",
+  // withCredentials: false,
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+});
+
+// customAxios.interceptors.response.use(response => {
+//       console.log('intercet res', response);
+//     }, error => {
+//       console.log('intercept error', error.response);
+//     }
+// );
+
 export default () => {
-    return axios.create({
-      baseURL: process.env.MIX_VUE_APP_URL + "/api",
-      // withCredentials: false,
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-    });
+    return customAxios;
 }
