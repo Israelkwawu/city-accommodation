d3d9d9d6153df294dff48f99f97eb49ca918682d
Israelkwawu <israel.kwawu@amalitech.com>
2023-03-09 16:50:01 +0000

    added cookie persistence

--- resources/js/store/index.js
+++ resources/js/store/index.js
@@ -1,13 +1,26 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import createPersistedState from 'vuex-persistedstate';
 import search from './modules/search';
 import property_type from './modules/property_type';
 import country from './modules/country';
 import property_list from './modules/property_list';
+import property_attribute from "./modules/property_attribute";
+import authentication from "./modules/authentication";
+import Cookies from 'js-cookie';
 
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
+  plugins:[
+    createPersistedState({
+      storage: {
+        getItem: key => Cookies.get(key),
+        setItem: (key, value) => Cookies.set(key, value, { expires: 1, secure: process.env.MIX_COOKIE_SECURE }),
+        removeItem: key => Cookies.remove(key)
+      }
+    })
+  ],
   state: {
   },
   getters: {},
@@ -18,7 +31,9 @@ const store = new Vuex.Store({
     search,
     property_type,
     country,
-    property_list
+    property_list,
+    property_attribute,
+    authentication,
   }
 })
 
